6579df916f0c180f8c503ea449cdb164dcfeea0b
Lotcelan <40437798+Lotcelan@users.noreply.github.com>
2023-10-12 14:28:23 +0200

    Update main.py
    Updated so it is clean and working :)

--- main.py
+++ main.py
@@ -24,10 +24,15 @@
 #   those images into a video with video.py.
 #   There you just have to link your images directory
 #   and it will create a video !
+#   You can also use a tool such as Flowframes to smooth the video :D
 #----------------------------------------------------
 
-import subprocess, time, requests, json, math, logging #Subprocess : Chrome command | time : time | requests/json : itinerary API | math : maths | logging : logging
+import subprocess, time, requests, json, math, logging, os #Subprocess : Chrome command | time : time | requests/json : itinerary API | math : maths | logging : logging
 from concurrent.futures import ThreadPoolExecutor #Threading
+from selenium import webdriver  
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 logging.warning("Started")
@@ -36,24 +41,112 @@ logging.warning("Started")
 
 #-------------------- CHANGE THIS -------------------
 
-save_path = "" #Insert your path (full path, not relative) to save images USING FORWARD SLASHES (E.g : C:/Users/Username/Desktop/images/)
-chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" #Path to google chrome.exe
+save_path = "C:/Path/To/images/" #Insert your path (full path, not relative) to save images USING FORWARD SLASHES (E.g : C:/Users/Username/Desktop/images/)
+chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe" #Path to google chrome.exe
+chrome_driver_path = "D:/Dev/Python/StreetViewClean/chromedriver.exe"
 key = "" #Get your MapQuest key : https://developer.mapquest.com/user/me/apps
 
 fromPlace = "" #The starting point : Address City Country
-toPlace = "" #The ending point : Address City Country
+toPlace = "e" #The ending point : Address City Country
 
 MAX_THREAD = 8 #Change this (Tip : from 5 to 15 it's ok, after that your PC will lag, a lot)
-VIRTUAL_TIME_BUDGET = 10000 #We found that it was the best time, but if you get blank/black images change this
+DELAY = 5 #We found that it was the best time, but if you get blank/black images change this
+
+# Not implemented yet :
+SAVE_FILE_NAME = "progress.save"
+SAVE_PROGRESS = True # If set to true, will create a save file containing the last route and where all threads left off
+USE_PROGRESS_IF_PRESENT = True # Will override the "fromPlace" and "toPlace" if a save file is present at the root directory of the python file
 
 #----------------------------------------------------
 
-def get_image(path, url, size="1920,1080"):
-    logging.warning("Processing : [File name] = {}; [Size] = {}; [URL] = {} !".format(path, size, url))
-    #It uses chrome headless mode to take screenshots
-    exec_query = subprocess.run([str(chrome_path), "--headless", "--disable-gpu", "--window-size=" + str(size), "--screenshot=" + str(save_path) + str(path), str(url), "--virtual-time-budget="+str(VIRTUAL_TIME_BUDGET)])
-    print("Query done.")
 
+def getNewDriver(chrome_path, chrome_driver_path, chrome_options):
+    driver = webdriver.Chrome(
+                executable_path=chrome_driver_path,
+                chrome_options=chrome_options
+                )
+    return driver
+
+
+def process_images(sub_coords, sub_id, starting_index, angle_mode, coords, size="1920,1080"):
+    chrome_options = Options()  
+    opts = ['--allow-running-insecure-content',
+            '--ignore-certificate-errors',
+            '--ignore-urlfetcher-cert-requests',
+            '--reduce-security-for-testing',
+            '--headless',
+            '--window-size=' + size,
+            '--disable-gpu',
+            '--run-all-compositor-stages-before-draw'
+            ]
+    for i in opts:
+        chrome_options.add_argument(i)
+    chrome_options.binary_location = chrome_path
+    driver = getNewDriver(chrome_path, chrome_driver_path, chrome_options)
+    driver.get('https://maps.google.com')
+    time.sleep(2)
+
+    button = driver.find_element(By.CSS_SELECTOR, "[aria-label='Tout accepter']")
+    button.click()
+
+    print(f"Thread {sub_id} ready !")
+    
+    name_idx = starting_index
+
+    for i in range(len(sub_coords)):
+        if angle_mode == "smooth":
+            angle = 0
+            if i == 0:
+                if sub_id == 0:
+                    # We're at the very beginning, so can not be "smooth"
+                    angle = getangle(sub_coords[i], coords[i + 1]) #Gets angle between the actual point and the next one
+                else:
+                    # We can get the last of the previous sub list
+                    angle = getangle(coords[starting_index - 1], sub_coords[i+1])
+            elif i == len(sub_coords) - 1:
+                if sub_id == MAX_THREADS-1:
+                    # We're at the very end, do nothing (TODO : look toward objective)
+                    pass
+                else:
+                    # We can get the first of the next sub list
+                    angle = getangle(sub_coords[i-1], coords[staring_index + len(sub_coords)])
+                    
+            else:
+                # General case
+                angle = getangle(sub_coords[i-1], sub_coords[i+1])
+            get_image(str(name_idx) + ".png", make_url(sub_coords[i], rot=str(angle)), driver)
+            name_idx += 1
+        # TODO : Sharp (but ugly so not doing now) 
+
+    driver.execute_script("window.location.href = 'about:blank'")
+    driver.quit()
+
+
+def get_image(path, url, driver):
+    logging.warning("Processing : [File name] = {}; [URL] = {} !".format(path, url))
+    #It uses chrome headless mode to take screenshots
+    try:
+        success = False
+        images_tries = 0
+        logging.warning("Processing : [File name] = {}; [URL] = {} !".format(path, url))
+
+        while success != True:
+            driver.execute_script("window.location.href = '" + str(url) + "'")
+            time.sleep(DELAY)
+            driver.save_screenshot(save_path+path)
+            file_size = os.path.getsize(save_path+path)
+
+            if file_size <= 200000 and images_tries <= 5:
+                os.remove(save_path+path)
+                images_tries += 1
+            else:
+                
+                success = True
+        print("Query done.")
+    except Exception as e:
+        logging.warning("Got an error while processing Processing : [File name] = {}; [URL] = {} ! ===> {}".format(path, url, e))
+    
+    return
 
 def make_url(coords, layer="c", map_ar="11", rot="0", tilt="0", zoom="0", pitch="0"):
     return "https://maps.google.com/maps?q=&layer={}&cbll={},{}&cbp={},{},{},{},{}".format(layer, coords[0], coords[1],map_ar, rot, tilt, zoom, pitch)
@@ -65,46 +158,57 @@ def getangle(ptA, ptB):
     r = math.degrees(  math.atan2(vector_c[1],vector_c[0]))
     return r
 
+def split_list_into_sublists(input_list, num_sublists):
+    sublist_length = len(input_list) // num_sublists
+    sublists = []
+    starting_indices = []
+
+    for i in range(0, len(input_list), sublist_length):
+        sublist = input_list[i:i + sublist_length]
+        sublists.append(sublist)
+        starting_indices.append(i)
+
+    # Ensure all elements are distributed among sublists
+    while len(sublists) > num_sublists:
+        last_sublist = sublists.pop()
+        sublists[-1].extend(last_sublist)
+
+    return sublists, starting_indices
 
 
 def main():
     coords = []
     #Api for itinerary
-    url = "http://www.mapquestapi.com/directions/v2/route?key={}&from={}&to={}&fullShape=true&shapeFormat=raw".format(key, fromPlace, toPlace)
+    url = "https://www.mapquestapi.com/directions/v2/route?key={}&from={}&to={}&fullShape=true&shapeFormat=raw&avoids=Tunnel".format(key, fromPlace, toPlace)
     r = requests.get(url)
     result = r.json()
 
     logging.warning("Response of route api : \n{}".format(result))
 
-    print("Amount of points : {}".format(len(result["route"]["shape"]["shapePoints"])))
+    try:
+        print("Amount of points : {}".format(len(result["route"]["shape"]["shapePoints"])//2))
 
-    for i in range(0, len(result["route"]["shape"]["shapePoints"]), 2):
-        #for debugging : print("Len : {} - Processing : {} and {}".format(len(result["route"]["shape"]["shapePoints"]),result["route"]["shape"]["shapePoints"][i], result["route"]["shape"]["shapePoints"][i + 1]))
-        coords.append((result["route"]["shape"]["shapePoints"][i], result["route"]["shape"]["shapePoints"][i + 1]))
+        for i in range(0, len(result["route"]["shape"]["shapePoints"]), 2):
+            #for debugging : print("Len : {} - Processing : {} and {}".format(len(result["route"]["shape"]["shapePoints"]),result["route"]["shape"]["shapePoints"][i], result["route"]["shape"]["shapePoints"][i + 1]))
+            coords.append((result["route"]["shape"]["shapePoints"][i], result["route"]["shape"]["shapePoints"][i + 1]))
     #Creates a list of tuples of coordinates (look at the API's doc to understand the result)
-
+    except Exception as e:
+        logging.warning("Got an error : the route probably doesn't exist")
+        print("y'a pas")
+        print(e)
+        return
+    
     logging.warning("Coords :\n{}".format(coords))
-    index = 0 #For names
     pool = ThreadPoolExecutor(max_workers=MAX_THREAD) #To avoid crashing your PC by spamming threads
 
     angle_mode = "smooth" #CHANGE THIS (if you want) It can either be smooth or sharp
 
-    if angle_mode == "smooth":
-        
-        for i in range(1, len(coords) - 1):
+    sublists, starting_indices = split_list_into_sublists(coords, MAX_THREAD)
 
-            angle = getangle(coords[i - 1], coords[i + 1]) #Gets angle between the actual point and the next one
-            pool.submit(get_image, "image_" + str(index) + "_rot=" + str(angle) + ".png",make_url(coords[i], rot=str(angle)))
-            index += 1
-
-    elif angle_mode == "sharp":
-
-            for i in range(len(coords) - 1):
-
-                angle = getangle(coords[i], coords[i + 1]) #Gets angle between the actual point and the next one
-                pool.submit(get_image, "image_" + str(index) + "_rot=" + str(angle) + ".png",make_url(coords[i], rot=str(angle)))
-                index += 1
-            
+    i = 0
+    for (sub_coords, start_idx) in zip(sublists, starting_indices):
+        pool.submit(process_images, sub_coords, i, start_idx, angle_mode, coords)
+        i += 1
 
     pool.shutdown(wait=True)
 
